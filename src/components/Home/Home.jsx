6553beb417c400ad93dc32a8c7a0f91990e37650
Abidur Rahman Arko <aroarko.sd@gmail.com>
2024-08-23 22:23:51 +0800

    all initialized with basic layout

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="font-rancho">All Coffee Section</div>;
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home/Home.jsx";
+import AddCoffee from "./components/AddCoffee/AddCoffee.jsx";
+import Root from "./components/Root/Root.jsx";
+import UpdateCoffee from "./components/UpdateCoffee/UpdateCoffee.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee></AddCoffee>,
+      },
+      {
+        path: "/updateCoffee",
+        element: <UpdateCoffee></UpdateCoffee>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
